49272456d586a8266728bfe3d32e55e6137ae14d
unknown <>
2016-07-08 20:33:31 +0800

    修改数据库信息存在db.php中。完善query.php中的bug

--- include/query.php
+++ include/query.php
@@ -10,13 +10,7 @@
     $recordsTotal = 0;
 
     //连接数据库
-    $dbms = "mysql";//选择数据库类型，MySQL
-    $host = "127.0.0.1"; //选择服务器
-    $userName = "wp";//用户名
-    $psw = "mkit";
-    $dbName = "test";//数据库名称
-    $dsn = "$dbms:host=$host;dbname=$dbName";
-    //include 'db.php';
+    include 'db.php';
     
     $pdo = new PDO($dsn, $userName, $psw);
     $total = "select count(uuid) total from topic";  
